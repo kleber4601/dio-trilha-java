33d2bde74ad9ca7fcd90c544e9403c9bafc8fda9
kleber Santos <ksd.campos@gmail.com>
2024-08-29 13:15:09 -0300

    operadores

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,10 +1,8 @@
-package teste;
 
-public class Teste1 {
+public class Operadores {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("teste");
 
 	}
 
