8c8c5ea45d1d8323be9f3be2062f5d7f0492182f
kleber Santos <ksd.campos@gmail.com>
2024-08-30 09:38:00 -0300

    novos cod

--- src/Operadores.java
+++ src/Operadores.java
@@ -3,6 +3,8 @@ public class Operadores {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Concatenação
 		String nomeCompleto = "Linguagem " + "Java";
 		System.out.println(nomeCompleto);
 		
@@ -20,6 +22,24 @@ public class Operadores {
 		
 		concatenacao = "1"+(1+1);
 		System.out.println(concatenacao);
+		
+		//Operadores Unários
+		int numero = 5;
+		System.out.println(numero);
+		
+		numero = - numero;
+		System.out.println(numero);
+		
+		numero = numero * -1;
+		System.out.println(numero);
+		
+		numero++;
+		System.out.println(numero);
+		
+		System.out.println(++numero);
+		
+		boolean variavel = true;
+		System.out.println(!variavel);
 
 	}
 
